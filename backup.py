12ced0a7fbe7f1b79073bcfcad3e553ce3deff4d
Tim Bolender <git@timbolender.de>
2015-06-12 15:16:42 +0200

    Added dry-run and completed testing.

--- backup.py
+++ backup.py
@@ -4,6 +4,8 @@
 from os import path
 from datetime import date, timedelta
 
+import subprocess
+
 def folder_time(folder):
     """Get day timestamp of a folder as date."""
     stamp = path.getmtime(folder)
@@ -15,19 +17,20 @@ def level_backup_needed(upper_first, lower_last, min_diff):
         return False
 
     if path.exists(upper_first):
-        # Nur durchführen wenn Zeitraum verstrichen
+        # Only perform if enough time passed
         time_upper = folder_time(upper_first)
         time_lower = folder_time(lower_last)
         return (time_lower - time_upper) >= min_diff
     else:
-        # Erstes Backup des höheren Levels
+        # First backup of higher level
         return True
 
 class RsnapshotManager(object):
+    _rsnapshot_command_template = 'rsnapshot -c rsnapshot.conf {action}'
     _daily_count = 5
     _weekly_count = 4
 
-    def __init__(self, backup_root='/home/tim/backup/rsnapshot/'):
+    def __init__(self, backup_root='/home/tim/backup/rsnapshot/', dry_run=False):
         self.daily_first = path.join(backup_root, 'daily.0')
         self.daily_last = path.join(backup_root, 'daily.' + str(self._daily_count-1))
         self.daily_diff = timedelta(days=1)
@@ -38,6 +41,8 @@ class RsnapshotManager(object):
 
         self.monthly_first = path.join(backup_root, 'monthly.0')
         self.monthly_diff = timedelta(days=28)
+
+        self.dry_run = dry_run
 
     @property
     def is_daily_needed(self):
@@ -58,14 +63,30 @@ class RsnapshotManager(object):
     def perform_sync(self):
         print('Performing sync')
 
+        if not self.dry_run:
+            command = self._rsnapshot_command_template.format(action='sync')
+            subprocess.check_call(command)
+
     def perform_daily(self):
         print('Performing daily backup')
+
+        if not self.dry_run:
+            command = self._rsnapshot_command_template.format(action='daily')
+            subprocess.check_call(command)
 
     def perform_weekly(self):
         print('Performing weekly backup')
 
+        if not self.dry_run:
+            command = self._rsnapshot_command_template.format(action='weekly')
+            subprocess.check_call(command)
+
     def perform_monthly(self):
         print('Performing monthly backup')
+
+        if not self.dry_run:
+            command = self._rsnapshot_command_template.format(action='monthly')
+            subprocess.check_call(command)
 
 def perform_backup(backup_manager=None):
     """Perform actual backup. Relies on a backup manager for information retrieving and backup performing."""
@@ -73,18 +94,17 @@ def perform_backup(backup_manager=None):
     if backup_manager is None:
         backup_manager = RsnapshotManager()
 
-    # Testen ob Backup generell nötig
+    # Test whether backup is needed in general
     if not backup_manager.is_daily_needed:
-        # Abbruch, heute schon Backup gemacht
         print('Abort: Daily backup already performed')
         return
 
-    # Sync durchführen
+    # Perform sync (actual backup)
     backup_manager.perform_sync()
 
-    # Ggf. Weekly Backup durchführen
+    # Perform weekly if needed
     if backup_manager.is_weekly_needed:
-        # Ggf. vorher Monthly Backup durchführen
+        # Perform monthly if needed
         if backup_manager.is_monthly_needed:
             backup_manager.perform_monthly()
 
